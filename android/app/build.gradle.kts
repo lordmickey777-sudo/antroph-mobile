53c62fdd00bf37bb822a34a0f736579e3f9542eb
Olabode Micheal AYOMIKUN <114963177+luli-tech@users.noreply.github.com>
2026-05-01 03:39:42 +0100

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,76 +1,3 @@
-import java.io.File
-import java.util.Properties
-
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
-}
-
-val keystorePropertiesFile = rootProject.file("key.properties")
-val keystoreProperties = Properties()
-if (keystorePropertiesFile.exists()) {
-    keystorePropertiesFile.inputStream().use { keystoreProperties.load(it) }
-}
-
-fun getSigningProperty(propertyKey: String, envKey: String): String? {
-    val fromFile = keystoreProperties.getProperty(propertyKey)
-    val fromEnv = System.getenv(envKey)
-    return (fromFile ?: fromEnv)?.takeIf { it.isNotBlank() }
-}
-
-val releaseStoreFilePath = getSigningProperty("storeFile", "ANDROID_KEYSTORE_PATH")
-val releaseStorePassword = getSigningProperty("storePassword", "ANDROID_KEYSTORE_PASSWORD")
-val releaseKeyAlias = getSigningProperty("keyAlias", "ANDROID_KEY_ALIAS")
-val releaseKeyPassword = getSigningProperty("keyPassword", "ANDROID_KEY_PASSWORD")
-
-val releaseStoreFile = releaseStoreFilePath
-    ?.takeIf { it.isNotBlank() }
-    ?.let { configuredPath ->
-        val configuredFile = File(configuredPath)
-        if (configuredFile.isAbsolute) configuredFile else rootProject.file(configuredPath)
-    }
-
-val isReleaseSigningConfigured = listOf(
-    releaseStoreFile,
-    releaseStorePassword,
-    releaseKeyAlias,
-    releaseKeyPassword,
-).all { property ->
-    when (property) {
-        is String -> property.isNotBlank()
-        is File -> property.exists()
-        else -> false
-    }
-}
-
-android {
-    namespace = "com.antroph.aura"
-    compileSdk = 36
-    ndkVersion = "27.0.12077973"
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.antroph.aura"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
 signingConfigs {
     create("release") {
         val storePath = System.getenv("CM_KEYSTORE_PATH")
@@ -98,8 +25,4 @@ buildTypes {
     release {
         signingConfig = signingConfigs.getByName("release")
     }
-}
-
-flutter {
-    source = "../.."
 }
