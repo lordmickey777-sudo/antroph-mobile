dda1de930d0a99ea391dfb35627f0e46100e2ef3
luli-tech <olabodemicheal5@gmail.com>
2026-05-02 19:28:43 +0100

    sdk upgrade

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,7 +49,7 @@ val isReleaseSigningConfigured = listOf(
 android {
     namespace = "com.antroph.aura"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
